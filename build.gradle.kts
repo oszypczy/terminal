0cc93515e5e3591a2e591bd3a1c3a4cffe53b1b8
Oliwier Szypczyn <oli.szypczyn@gmail.com>
2026-03-04 21:17:30 +0100

    Update build.gradle.kts to configure Java compatibility and Kotlin compiler options

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,18 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     testImplementation("org.assertj:assertj-core:3.25.1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
+    }
 }
 
 tasks.test {
